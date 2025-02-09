a0055588ad06e4f6d82496cfd38c932e3525fe41
Paréj Bence <parej.bence@novaservices.hu>
2025-02-09 19:55:58 +0100

    change lombok dependency

--- services/notification/src/main/java/com/microdemo/microservices/kafka/NotificationConsumer.java
+++ services/notification/src/main/java/com/microdemo/microservices/kafka/NotificationConsumer.java
@@ -26,55 +26,47 @@ public class NotificationConsumer {
 
     private final EmailService emailService;
 
-    @Service
-    @Slf4j
-    @RequiredArgsConstructor
-    public class NotificationsConsumer {
+    @KafkaListener(topics = "payment-topic")
+    public void consumePaymentSuccessNotifications(PaymentConfirmation paymentConfirmation) throws MessagingException {
+        log.info(format("Consuming the message from payment-topic Topic:: %s", paymentConfirmation));
+        repository.save(
+                Notification.builder()
+                        .type(PAYMENT_CONFIRMATION)
+                        .notificationDate(LocalDateTime.now())
+                        .paymentConfirmation(paymentConfirmation)
+                        .build()
+        );
 
-        private final NotificationRepository repository;
-
-        //        private final EmailService emailService;
-        @KafkaListener(topics = "payment-topic")
-        public void consumePaymentSuccessNotifications(PaymentConfirmation paymentConfirmation) throws MessagingException {
-            log.info(format("Consuming the message from payment-topic Topic:: %s", paymentConfirmation));
-            repository.save(
-                    Notification.builder()
-                            .type(PAYMENT_CONFIRMATION)
-                            .notificationDate(LocalDateTime.now())
-                            .paymentConfirmation(paymentConfirmation)
-                            .build()
-            );
-
-            var customerName = paymentConfirmation.customerFirstName() + " " + paymentConfirmation.customerLastName();
-            emailService.sendPaymentSuccessEmail(
-                    paymentConfirmation.customerEmail(),
-                    customerName,
-                    paymentConfirmation.amount(),
-                    paymentConfirmation.orderReference()
-            );
-        }
-
-        @KafkaListener(topics = "order-topic")
-        public void consumeOrderConfirmationNotifications(OrderConfirmation orderConfirmation) throws MessagingException {
-            log.info(format("Consuming the message from order-topic Topic:: %s", orderConfirmation));
-            repository.save(
-                    Notification.builder()
-                            .type(ORDER_CONFIRMATION)
-                            .notificationDate(LocalDateTime.now())
-                            .orderConfirmation(orderConfirmation)
-                            .build()
-            );
-
-
-            var customerName = orderConfirmation.customer().firstName() + " " + orderConfirmation.customer().lastName();
-            emailService.sendOrderConfirmationEmail(
-                    orderConfirmation.customer().email(),
-                    customerName,
-                    orderConfirmation.totalAmount(),
-                    orderConfirmation.orderReference(),
-                    orderConfirmation.products()
-            );
-        }
-
+        var customerName = paymentConfirmation.customerFirstName() + " " + paymentConfirmation.customerLastName();
+        emailService.sendPaymentSuccessEmail(
+                paymentConfirmation.customerEmail(),
+                customerName,
+                paymentConfirmation.amount(),
+                paymentConfirmation.orderReference()
+        );
     }
+
+    @KafkaListener(topics = "order-topic")
+    public void consumeOrderConfirmationNotifications(OrderConfirmation orderConfirmation) throws MessagingException {
+        log.info(format("Consuming the message from order-topic Topic:: %s", orderConfirmation));
+        repository.save(
+                Notification.builder()
+                        .type(ORDER_CONFIRMATION)
+                        .notificationDate(LocalDateTime.now())
+                        .orderConfirmation(orderConfirmation)
+                        .build()
+        );
+
+
+        var customerName = orderConfirmation.customer().firstName() + " " + orderConfirmation.customer().lastName();
+        emailService.sendOrderConfirmationEmail(
+                orderConfirmation.customer().email(),
+                customerName,
+                orderConfirmation.totalAmount(),
+                orderConfirmation.orderReference(),
+                orderConfirmation.products()
+        );
+    }
+
+
 }
